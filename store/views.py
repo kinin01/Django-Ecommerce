58f90245dd0cc2e9ac872125f52da2bf7bdb1724
kinin01 <kiningumbuvi@gmail.com>
2024-03-29 22:28:51 +0000

    created order and order items

--- store/views.py
+++ store/views.py
@@ -1,15 +1,18 @@
-from django.shortcuts import render,redirect
-from .models import Product,Category, Profile
+from django.shortcuts import render, redirect
+from .models import Product, Category, Profile
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .forms import SignUpForm,UpdateUserForm, ChangePasswordForm,UserInfoForm
+from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm, UserInfoForm
+
+from payment.forms import ShippingForm
+from payment.models import ShippingAddress
+
 from django import forms
 from django.db.models import Q
 import json
 from cart.cart import Cart
-
 
 
 def search(request):
@@ -28,26 +31,29 @@ def search(request):
 		return render(request, "search.html", {})	
 
 
-
-
-
-
-def Update_info(request):
+def update_info(request):
 	if request.user.is_authenticated:
+		# Get Current User
 		current_user = Profile.objects.get(user__id=request.user.id)
+		# Get Current User's Shipping Info
+		shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
+		
+		# Get original User Form
 		form = UserInfoForm(request.POST or None, instance=current_user)
-
-		if form.is_valid():
+		# Get User's Shipping Form
+		shipping_form = ShippingForm(request.POST or None, instance=shipping_user)		
+		if form.is_valid() or shipping_form.is_valid():
+			# Save original form
 			form.save()
+			# Save shipping form
+			shipping_form.save()
+
 			messages.success(request, "Your Info Has Been Updated!!")
 			return redirect('home')
-		return render(request, "update_info.html", {'form':form})
+		return render(request, "update_info.html", {'form':form, 'shipping_form':shipping_form})
 	else:
 		messages.success(request, "You Must Be Logged In To Access That Page!!")
 		return redirect('home')
-
-
-
 
 
 
@@ -73,13 +79,6 @@ def update_password(request):
 	else:
 		messages.success(request, "You Must Be Logged In To View That Page...")
 		return redirect('home')
-
-
-
-
-
-
-
 def update_user(request):
 	if request.user.is_authenticated:
 		current_user = User.objects.get(id=request.user.id)
@@ -97,29 +96,27 @@ def update_user(request):
 		return redirect('home')
 
 
+def category_summary(request):
+	categories = Category.objects.all()
+	return render(request, 'category_summary.html', {"categories":categories})	
 
 def category(request, foo):
-#replace hypens with spaces.
-   foo = foo.replace('-', '')
-
-   try:
-   
-      category = Category.objects.get(name=foo)
-      products = Product.objects.filter(category=category)
-      return render(request, 'category.html', {'products': products, 'category':category})
-
-   except: 
-      messages.success(request, ("That Category Doesn't Exist...."))  
-      return redirect('home')
-      
+	# Replace Hyphens with Spaces
+	foo = foo.replace('-', ' ')
+	# Grab the category from the url
+	try:
+		# Look Up The Category
+		category = Category.objects.get(name=foo)
+		products = Product.objects.filter(category=category)
+		return render(request, 'category.html', {'products':products, 'category':category})
+	except:
+		messages.success(request, ("That Category Doesn't Exist..."))
+		return redirect('home')
 
 
-
-
-
-def product(request, pk):
-    product = Product.objects.get(id=pk)
-    return render(request, 'product.html', {'product': product})
+def product(request,pk):
+	product = Product.objects.get(id=pk)
+	return render(request, 'product.html', {'product':product})
 
 
 def home(request):
@@ -128,8 +125,7 @@ def home(request):
 
 
 def about(request):
-    return render(request, 'about.html', {})
-
+	return render(request, 'about.html', {})	
 
 def login_user(request):
 	if request.method == "POST":
@@ -165,9 +161,10 @@ def login_user(request):
 
 
 def logout_user(request):
-    logout(request)
-    messages.success(request, ("You have been  logged out...Thanks!!!"))
-    return redirect('home')
+	logout(request)
+	messages.success(request, ("You have been logged out...Thanks for stopping by..."))
+	return redirect('home')
+
 
 
 def register_user(request):
@@ -188,5 +185,3 @@ def register_user(request):
 			return redirect('register')
 	else:
 		return render(request, 'register.html', {'form':form})
-
-
