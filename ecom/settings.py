b092bea48e2002751935ea221766960d56d0225f
kinin01 <kiningumbuvi@gmail.com>
2024-06-22 20:36:33 +0100

    added varaible security key

--- ecom/settings.py
+++ ecom/settings.py
@@ -20,7 +20,7 @@ ENVIRONMENT =os.environ.get('ENVIRONMENT', default='production')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if ENVIRONMENT == 'development':
